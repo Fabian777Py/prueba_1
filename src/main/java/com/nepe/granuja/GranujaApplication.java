d0c1f5ec94216d0ee15a036f7de8d2dac79c3b12
Fabian777Py <fabianacosta7712@gmail.com>
2025-06-10 17:09:39 -0500

    inicial

---
+++ src/main/java/com/nepe/granuja/GranujaApplication.java
@@ -0,0 +1,13 @@
+package com.nepe.granuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranujaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranujaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nepe/granuja/GranujaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nepe.granuja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranujaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
